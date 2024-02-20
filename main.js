34b5d61c1e5b3e669b84dd494eb11793f51eba7e
LAPTOP-5Q00H7G4\regis <weijung0923@gmail.com>
2024-02-20 13:27:32 +0800

    第一天的地圖練習code.

---
+++ main.js
@@ -0,0 +1,29 @@
+const map = new ol.Map({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    target: 'map',
+    view: new ol.View({
+        projection: "EPSG:3857",
+        center: ol.proj.fromLonLat([120.846642, 23.488793]),
+        zoom: 7.5,
+        maxZoom: 20,
+        minZoom: 5,
+        enableRotation: false,
+    }),
+    controls: []
+});
+
+let point = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [
+            new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([121, 23]))
+            })
+        ]
+    })
+}); 
+
+map.addLayer(point);
