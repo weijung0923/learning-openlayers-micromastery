c3c8a71b29722f001de27e0f4d5f91e36b3f9060
LAPTOP-5Q00H7G4\regis <weijung0923@gmail.com>
2024-03-04 21:01:01 +0800

    新增測量工具

---
+++ main.js
@@ -0,0 +1,126 @@
+const map = new ol.Map({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    target: 'map',
+    view: new ol.View({
+        projection: "EPSG:3857",
+        center: ol.proj.fromLonLat([120.846642, 23.488793]),
+        zoom: 7.5,
+        maxZoom: 20,
+        minZoom: 5,
+        enableRotation: false,
+    }),
+    controls: []
+});
+
+// 1.source共用
+const source = new ol.source.Vector();
+const vector = new ol.layer.Vector({
+    source: source,
+    style: {
+        'fill-color': 'rgba(255, 255, 255, 0.2)',
+        'stroke-color': '#ffcc33',
+        'stroke-width': 2,
+        'circle-radius': 7,
+        'circle-fill-color': '#ffcc33',
+    },
+});
+map.addLayer(vector)
+
+// Currently drawn feature.
+let sketch;
+// The measure tooltip element.
+let measureTooltipElement;
+// Overlay to show the measurement.
+let measureTooltip;
+
+// 計算距離
+const formatLength = function (line) {
+    const length = ol.sphere.getLength(line);
+    let output;
+    if (length > 100) {
+        output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+    } else {
+        output = Math.round(length * 100) / 100 + ' ' + 'm';
+    }
+    return output;
+};
+
+// 增加互動
+function addInteraction() {
+    let draw = new ol.interaction.Draw({
+        // 1.source共用
+        source: source,
+        type: 'LineString',
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)',
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'rgba(0, 0, 0, 0.5)',
+                lineDash: [10, 10],
+                width: 2,
+            }),
+            image: new ol.style.Circle({
+                radius: 5,
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.7)',
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 255, 255, 0.2)',
+                }),
+            }),
+        }),
+    });
+    map.addInteraction(draw);
+
+    createMeasureTooltip();
+
+    let listener;
+    draw.on('drawstart', function (evt) {
+        // set sketch
+        sketch = evt.feature;
+        let tooltipCoord = evt.coordinate;
+
+        listener = sketch.getGeometry().on('change', function (evt) {
+            const geom = evt.target;
+            let output = formatLength(geom);
+            tooltipCoord = geom.getLastCoordinate();
+            measureTooltipElement.innerHTML = output; // 顯示計算完的長度
+            measureTooltip.setPosition(tooltipCoord); // 更新提示框的位置
+        });
+    });
+
+    draw.on('drawend', function () {
+        measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
+        measureTooltip.setOffset([0, -7]);
+        // unset sketch
+        sketch = null;
+        // unset tooltip so that a new one can be created
+        measureTooltipElement = null;
+        createMeasureTooltip();
+        ol.Observable.unByKey(listener);
+    });
+}
+
+// 顯示測量距離
+function createMeasureTooltip() {
+    if (measureTooltipElement) {
+        measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+    }
+    measureTooltipElement = document.createElement('div');
+    measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+    measureTooltip = new ol.Overlay({
+        element: measureTooltipElement,
+        offset: [0, -15],
+        positioning: 'bottom-center',
+        stopEvent: false,
+        insertFirst: false,
+    });
+    map.addOverlay(measureTooltip);
+}
+
+addInteraction();
